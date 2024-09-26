66e32ad4399dce84aac0ffb984fc0583eb2b62ea
aadhi-padmanaban <padmanaban946@gmail.com>
2024-09-27 00:05:00 +0530

    search component
    searchbar

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Screen from '../components/Screen';
-import {View, Text} from 'react-native';
-import Colors from '../config/Colors';
+import React from "react";
+import Screen from "../components/Screen";
+import { View, Text } from "react-native";
+import Colors from "../config/Colors";
+import SearchBar from "../components/SearchBar";
 
-const Home=()=>{
-    return(
-        <Screen>
-        <StatusBar backgroundColor={Colors.red} />
-            <View><Text>HI</Text></View>
-        </Screen>
-    );
-}
+const Home = () => {
+  return (
+    <Screen>
+      <StatusBar backgroundColor={Colors.red} />
+      <SearchBar />
+    </Screen>
+  );
+};
 export default Home;
