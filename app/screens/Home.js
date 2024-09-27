17eaf45a40f0825e0eadc2cf67dba1d95fa56805
aadhi-padmanaban <padmanaban946@gmail.com>
2024-09-28 00:12:08 +0530

    Bottomtab icon added
    bottom tab icons added

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Screen from "../components/Screen";
-import { View, Text } from "react-native";
 import Colors from "../config/Colors";
 import SearchBar from "../components/SearchBar";
+import Category from "../components/Category";
 
 const Home = () => {
   return (
     <Screen>
       <StatusBar backgroundColor={Colors.red} />
       <SearchBar />
+      <Category />
     </Screen>
   );
 };
